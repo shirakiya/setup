54c68c2eff7a1f9ba8530f2ec062409e16e0a08e
yoshihiko.shiraki <shirakiya.pv@gmail.com>
2016-01-06 11:30:20 +0900

    update brewfile

--- brewcaskfile.sh
+++ brewcaskfile.sh
@@ -9,7 +9,6 @@ brew cask install appcleaner
 brew cask install coteditor
 brew cask install evernote
 brew cask install firefox
-brew cask install dropbox
 brew cask install google-chrome
 brew cask install google-japanese-ime
 brew cask install hipchat
@@ -17,11 +16,11 @@ brew cask install iterm2
 brew cask install java
 brew cask install karabiner
 brew cask install kobito
-brew cask install virtualbox
-brew cask install vagrant
+brew cask install licecap
 brew cask install seil
 brew cask install sequel-pro
 brew cask install skype
 brew cask install slack
+brew cask install vagrant
+brew cask install virtualbox
 brew cask install vlc
-brew cask install xtrafinder
--- brewfile.sh
+++ brewfile.sh
@@ -8,6 +8,7 @@ brew install wget
 brew install tree
 brew install ag
 brew install jq
+brew install gibo
 brew install git
 brew install tig
 brew install zsh
@@ -16,14 +17,17 @@ brew install zsh-syntax-highlighting
 brew install source-highlight
 brew install vim --with-lua
 brew install tmux
+brew install brew-cask
 brew install mysql
 brew install redis
+brew install mongodb
 brew install nginx
 brew install nodebrew
 brew install go
 brew install pyenv pyenv-virtualenv
 brew install plenv perl-build
 brew install rbenv ruby-build
+brew install scalaenv
 brew install ansible
 brew install reattach-to-user-namespace
 brew install awscli
