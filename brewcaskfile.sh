2ea48a0372a96c54b7b118132cd3aa3a1af44d23
yoshihiko.shiraki <shirakiya.pv@gmail.com>
2016-06-29 10:39:55 +0900

    comment out apps got via AppStore

--- brewcaskfile.sh
+++ brewcaskfile.sh
@@ -6,10 +6,10 @@ if ! brew tap | grep caskroom/cask > /dev/null; then
 fi
 
 brew cask install appcleaner
-brew cask install coteditor
 brew cask install dropbox
 brew cask install evernote
 brew cask install firefox
+brew cask install flowdock
 brew cask install google-chrome
 brew cask install google-japanese-ime
 brew cask install iterm2
@@ -21,10 +21,15 @@ brew cask install licecap
 brew cask install seil
 brew cask install sequel-pro
 brew cask install skype
-brew cask install slack
 brew cask install vagrant
 brew cask install virtualbox
 brew cask install vlc
 
 # インストール時に使用したzipファイルを削除する
 brew cask cleanup
+
+# Get app via App Store
+# - coteditor
+# - skitch
+# - slack
+# - Wunderlist
